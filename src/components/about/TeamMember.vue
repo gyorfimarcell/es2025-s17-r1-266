213e29bbd4e556142c976ca40a4cb5bf7ee568f5
Györfi Marcell <gyorfimarcell@gmail.com>
2024-02-25 14:03:01 +0100

    About us Team + CTA

---
+++ src/components/about/TeamMember.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+defineProps({
+    name: String,
+    role: String,
+    image: String
+});
+</script>
+
+<template>
+    <div class="member">
+        <img :src="'/images/' + image" :alt="name">
+        <p class="name">{{ name }}</p>
+        <p class="role">{{ role }}</p>
+    </div>
+</template>
+
+<style scoped>
+.member {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+
+    text-align: center;
+}
+
+img {
+    width: 96px;
+}
+
+.name {
+    font-weight: bold;
+}
+
+.role {
+    color: var(--text-muted-color);
+}
+</style>
--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,8 +1,95 @@
 <script setup lang="ts">
+import TeamMember from "../components/about/TeamMember.vue";
 </script>
 
 <template>
-    <p>About us</p>
+    <div class="title">
+        <h1>About Us</h1>
+        <p>Embracing the Joy of Dining</p>
+    </div>
+    <div class="team">
+        <h1>Meet our team</h1>
+        <p>The Culinary Visionaries Behind DineEase</p>
+        <div class="members">
+            <TeamMember name="Elena Martinez" role="Founder & CEO" image="person1.png"></TeamMember>
+            <TeamMember name="Raj Singh" role="Chief Culinary Curator" image="person3.png"></TeamMember>
+            <TeamMember name="Sophia Dawson" role="Director of Community Engagement" image="person2.png"></TeamMember>
+            <TeamMember name="Lucas Kim" role="Head of Digital Strategy" image="person4.png"></TeamMember>
+            <TeamMember name="Aisha Abdi" role="Marketing & Brand Manager" image="person5.png"></TeamMember>
+        </div>
+    </div>
+    <div class="connect">
+        <h1>Connect with DineEase</h1>
+        <p>
+            Hungry for more? Whether you have questions, feedback, or simply wish to share your culinary story, we're eager
+            to hear from you. Dive deeper into the DineEase experience and let's make every meal memorable.
+            Button text: Connect With DineEase
+        </p>
+        <button>Connect With DineEase</button>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.title,
+.team {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+}
+
+.team {
+    padding: 64px var(--lg-padding-x);
+}
+
+.members {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 48px 0;
+
+    margin-top: 48px;
+}
+
+.member {
+    flex-basis: 50%;
+}
+
+.connect {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 32px;
+
+    padding: 120px var(--sm-padding-x);
+
+    background-color: var(--primary-color);
+    color: white;
+    text-align: center;
+}
+
+.connect button {
+    padding: 12px;
+    border-radius: var(--card-radius);
+
+    background-color: white;
+    color: var(--primary-color);
+    font-weight: bold;
+}
+
+@media screen and (min-width: 992px) {
+    .team {
+        padding-left: 198px;
+        padding-right: 198px;
+    }
+
+    .member {
+        flex-basis: calc(100% / 3);
+    }
+
+    .connect {
+        padding-left: 386px;
+        padding-right: 386px;
+    }
+}
+</style>
